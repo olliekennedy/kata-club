197eb664e610ee7a41f246121b71de1342818443
Ollie Kennedy <okenned3@ford.com>
2025-09-30 10:24:18 +0100

    x

--- src/test/kotlin/BowlingScorecardTest.kt
+++ src/test/kotlin/BowlingScorecardTest.kt
@@ -10,13 +10,21 @@ class BowlingScorecardTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = [1,2,3,4,5,6,7,8,9])
-    fun `ivan improves`(score: Int) {
+    @ValueSource(ints = [1, 2, 3, 4, 5, 6, 7, 8, 9])
+    fun `can score a frame where only one throw is a hit`(score: Int) {
         assertEquals(score, scoreFor("$score- -- -- -- -- -- -- -- -- ---"))
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 3, 4])
+    fun `can score a frame where both throws are a hit`(score: Int) {
+        assertEquals(score * 2, scoreFor("$score$score -- -- -- -- -- -- -- -- ---"))
+    }
+
     private fun scoreFor(scorecard: String): Int {
-        if(scorecard[0].isDigit()) return scorecard[0].digitToInt()
-        return 0
+        var score = 0
+        if (scorecard[0].isDigit()) score += scorecard[0].digitToInt()
+        if (scorecard[1].isDigit()) score += scorecard[1].digitToInt()
+        return score
     }
 }
