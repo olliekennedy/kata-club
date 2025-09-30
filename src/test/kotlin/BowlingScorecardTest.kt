811c70c3c22414b2685c8decee6de46c128a621d
Ollie Kennedy <okenned3@ford.com>
2025-09-30 10:19:35 +0100

    x

--- src/test/kotlin/BowlingScorecardTest.kt
+++ src/test/kotlin/BowlingScorecardTest.kt
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import kotlin.test.assertEquals
 
 class BowlingScorecardTest {
@@ -7,13 +9,14 @@ class BowlingScorecardTest {
         assertEquals(0, scoreFor("-- -- -- -- -- -- -- -- -- ---"))
     }
 
-    @Test
-    fun `can score ivan style game`() {
-        assertEquals(1, scoreFor("1- -- -- -- -- -- -- -- -- ---"))
+    @ParameterizedTest
+    @ValueSource(ints = [1,2,3,4,5,6,7,8,9])
+    fun `ivan improves`(score: Int) {
+        assertEquals(score, scoreFor("$score- -- -- -- -- -- -- -- -- ---"))
     }
 
     private fun scoreFor(scorecard: String): Int {
-        if (scorecard.contains("1")) return 1
+        if(scorecard[0].isDigit()) return scorecard[0].digitToInt()
         return 0
     }
 }
