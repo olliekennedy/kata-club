6509b970d049ad4dbe66d58f4f8ebe6c3bad0474
Ollie Kennedy <okenned3@ford.com>
2025-10-14 14:22:42 +0100

    checkout

---
+++ src/test/kotlin/CheckoutTest.kt
@@ -0,0 +1,73 @@
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.argumentSet
+import org.junit.jupiter.params.provider.MethodSource
+import kotlin.test.assertEquals
+
+class CheckoutTest {
+    companion object {
+        @JvmStatic
+        fun expectations(): List<Arguments.ArgumentSet?> {
+            return listOf(
+                argumentSet("it costs nothing to buy nothing", listOf<String>(), 0),
+                argumentSet("buy a single item", listOf("A"), 50),
+                argumentSet("buy multiple of a single type of item", listOf("A", "A"), 100),
+                argumentSet("buy two different types of item", listOf("A", "B"), 80),
+                argumentSet("buy multiple types of item", listOf("A", "B", "C"), 100),
+                argumentSet("items not from the shop cost nothing", listOf("?"), 0),
+                argumentSet("buy an item on offer", listOf("A", "A", "A"), 130),
+                argumentSet("buy an item on offer and one additional", listOf("A", "A", "A", "A"), 180),
+                argumentSet("buy an item on offer multiple times", listOf("A", "A", "A", "A", "A", "A", "A"), 310),
+                argumentSet("buy another item on offer", listOf("B", "B"), 50),
+            )
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("expectations")
+    fun `it costs nothing to buy nothing`(items: List<String>, expected: Int) {
+        val basket = Basket()
+
+        items.forEach { basket.add(it) }
+
+        assertEquals(expected, basket.total())
+    }
+}
+
+class Basket {
+    val items = mutableMapOf<String, Int>()
+
+    data class Price(
+        val normal: Int,
+        val specialOffer: SpecialOffer? = null,
+    )
+
+    data class SpecialOffer(
+        val price: Int,
+        val quantity: Int,
+    )
+
+    val catalogue = mapOf(
+        "A" to Price(50, SpecialOffer(130, 3)),
+        "B" to Price(30, SpecialOffer(50, 2)),
+        "C" to Price(20),
+    )
+
+    fun total(): Int {
+        return items
+            .map { (item, count) ->
+                catalogue[item]?.let { price ->
+                    price.specialOffer?.let { specialOffer ->
+                        val howManySpecialOffers = count / specialOffer.quantity
+                        val countLeft = count % specialOffer.quantity
+                        (specialOffer.price * howManySpecialOffers) + (price.normal * countLeft)
+                    } ?: (price.normal * count)
+                } ?: 0
+            }
+            .sum()
+    }
+
+    fun add(sku: String) {
+        items[sku] = (items[sku] ?: 0) + 1
+    }
+}
