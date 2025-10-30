22d40d6ded7e4a0f93d8a35e385ed1cafea10aee
Ollie Kennedy <okenned3@ford.com>
2025-10-30 14:16:48 +0000

    get a voting log

--- src/test/kotlin/LunchLeagueTest.kt
+++ src/test/kotlin/LunchLeagueTest.kt
@@ -1,3 +1,4 @@
+import com.natpryce.hamkrest.allElements
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
@@ -35,8 +36,26 @@ class LunchLeagueTest {
         assertThat(leagueTable.results(), equalTo(listOf(Entry("Canteen", 6.3))))
     }
 
+    @Test
+    fun `can get a voting log for a restaurant`() {
+        val leagueTable = LeagueTable()
+
+        leagueTable.vote("Canteen", 1)
+        leagueTable.vote("Canteen", 10)
+        leagueTable.vote("Canteen", 8)
+        leagueTable.vote("Canteen", 6)
+
+        assertThat(
+            leagueTable.votingLogFor("Canteen"),
+            List<Vote>::containsAll,
+            listOf(Vote(1), Vote(10), Vote(8), Vote(6)),
+        )
+    }
+
 //    TODO - add a voter name
 }
+
+data class Vote(val rating: Int)
 
 class LeagueTable() {
     fun vote(name: String, rating: Int) {
@@ -53,6 +72,10 @@ class LeagueTable() {
         votes.entries.map { (name, votes) ->
             Entry(name = name, rating = votes.average())
         }.toList()
+
+    fun votingLogFor(restaurantName: String): List<Vote> {
+        return votes.getOrElse(restaurantName) { listOf() }.map { Vote(it) }
+    }
 }
 
 private fun MutableList<Int>.average(): Double = sum().div(size.toDouble()).roundedToOneDecimalPlace()
