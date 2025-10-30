01dc6ff194cc2dd25cb5571a392d44d554c4205c
Ollie Kennedy <okenned3@ford.com>
2025-10-30 21:41:01 +0000

    refactor

--- src/test/kotlin/LunchLeagueTest.kt
+++ src/test/kotlin/LunchLeagueTest.kt
@@ -5,59 +5,69 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 
 class LunchLeagueTest {
+    val leagueTable = LeagueTable()
 
     @Test
-    fun `returns an empty league table`() {
-        val leagueTable = LeagueTable()
-
-        assertThat(leagueTable.results(), equalTo(listOf()))
-    }
-
-    @Test
-    fun `can add votes for a restaurant`() {
-        val leagueTable = LeagueTable()
-
-        leagueTable.vote("Canteen", "me", 8)
-        leagueTable.vote("Canteen", "bob", 6)
-
-        assertThat(leagueTable.results(), equalTo(listOf(Entry("Canteen", 7.0))))
-    }
-
-    @Test
-    fun `can add multiple votes for a restaurant`() {
-        val leagueTable = LeagueTable()
-
+    fun `can calculate average ratings for each restaurant`() {
         leagueTable.vote("Canteen", "me", 1)
         leagueTable.vote("Canteen", "bob", 10)
         leagueTable.vote("Canteen", "sandra", 8)
         leagueTable.vote("Canteen", "brenda", 6)
+        leagueTable.vote("Randy's Wing Bar", "me", 1)
+        leagueTable.vote("Randy's Wing Bar", "bob", 2)
+        leagueTable.vote("Randy's Wing Bar", "sandra", 3)
+        leagueTable.vote("Randy's Wing Bar", "brenda", 4)
+        leagueTable.vote("Hackney Bridge", "me", 3)
+        leagueTable.vote("Hackney Bridge", "bob", 4)
+        leagueTable.vote("Hackney Bridge", "sandra", 9)
+        leagueTable.vote("Hackney Bridge", "brenda", 10)
 
-        assertThat(leagueTable.results(), equalTo(listOf(Entry("Canteen", 6.3))))
+        assertThat(
+            leagueTable.results(),
+            equalTo(
+                listOf(
+                    Entry("Canteen", 6.3),
+                    Entry("Randy's Wing Bar", 2.5),
+                    Entry("Hackney Bridge", 6.5),
+                )
+            )
+        )
     }
 
     @Test
-    fun `vote for multiple restaurants`() {
-        val leagueTable = LeagueTable()
+    fun `can get a voting log for a restaurant`() {
+        leagueTable.vote("Canteen", "me", 1)
+        leagueTable.vote("Canteen", "bob", 10)
+        leagueTable.vote("Canteen", "sandra", 8)
+        leagueTable.vote("Canteen", "brenda", 6)
+        leagueTable.vote("Randy's Wing Bar", "me", 1)
+        leagueTable.vote("Randy's Wing Bar", "bob", 2)
+        leagueTable.vote("Randy's Wing Bar", "sandra", 3)
+        leagueTable.vote("Randy's Wing Bar", "brenda", 4)
+        leagueTable.vote("Hackney Bridge", "me", 3)
+        leagueTable.vote("Hackney Bridge", "bob", 4)
+        leagueTable.vote("Hackney Bridge", "sandra", 9)
+        leagueTable.vote("Hackney Bridge", "brenda", 10)
 
-        leagueTable.vote("Canteen", "me", 6)
-        leagueTable.vote("Canteen", "bob", 8)
-        leagueTable.vote("Randy's Wing Bar", "me", 8)
-        leagueTable.vote("Hackney Bridge", "me", 6)
-
-        assertThat(leagueTable.results(), equalTo(listOf(Entry("Canteen", 7.0), Entry("Randy's Wing Bar", 8.0), Entry("Hackney Bridge", 6.0))))
         assertThat(
             leagueTable.votingLogFor("Canteen"),
             List<Vote>::containsAll,
-            listOf(Vote("me", 6), Vote("bob", 8)),
+            listOf(Vote("me", 1), Vote("bob", 10), Vote("sandra", 8), Vote("brenda", 6)),
         )
-        assertThat(leagueTable.votingLogFor("Randy's Wing Bar"), equalTo(listOf(Vote("me", 8))))
-        assertThat(leagueTable.votingLogFor("Hackney Bridge"), equalTo(listOf(Vote("me", 6))))
+        assertThat(
+            leagueTable.votingLogFor("Canteen"),
+            List<Vote>::containsAll,
+            listOf(Vote("me", 1), Vote("bob", 10), Vote("sandra", 8), Vote("brenda", 6)),
+        )
+        assertThat(
+            leagueTable.votingLogFor("Canteen"),
+            List<Vote>::containsAll,
+            listOf(Vote("me", 1), Vote("bob", 10), Vote("sandra", 8), Vote("brenda", 6)),
+        )
     }
 
     @Test
     fun `someone can update their rating for a restaurant`() {
-        val leagueTable = LeagueTable()
-
         leagueTable.vote("Canteen", "me", 6)
         leagueTable.vote("Canteen", "me", 8)
 
@@ -65,31 +75,28 @@ class LunchLeagueTest {
     }
 }
 
-data class Vote(val voter: String, val rating: Int)
-
 class LeagueTable() {
-    fun vote(restaurant: String, voter: String, rating: Int) {
-        if (restaurant !in votes) {
-            votes[restaurant] = mutableMapOf(voter to rating)
-        } else {
-            votes[restaurant]?.set(voter, rating)
-        }
-    }
-
     val votes = mutableMapOf<String, MutableMap<String, Int>>()
 
-    fun results(): List<Any> =
+    fun vote(restaurant: String, voter: String, rating: Int) {
+        val restaurantVotes = votes.getOrPut(restaurant) { mutableMapOf() }
+        restaurantVotes[voter] = rating
+    }
+
+    fun results(): List<Entry> =
         votes.entries.map { (name, votes) ->
-            Entry(name = name, rating = votes.map { it.value }.average())
+            val rating = votes.map { it.value }.average()
+            Entry(name, rating)
         }.toList()
 
-    fun votingLogFor(restaurantName: String): List<Vote> {
-        return votes.getOrElse(restaurantName) { mapOf() }.map { Vote(it.key, it.value) }
-    }
+    fun votingLogFor(restaurantName: String): List<Vote> =
+        votes
+            .getOrElse(restaurantName) { mapOf() }
+            .map { Vote(it.key, it.value) }
 }
 
 private fun List<Int>.average(): Double = sum().div(size.toDouble()).roundedToOneDecimalPlace()
-
 private fun Double.roundedToOneDecimalPlace(): Double = BigDecimal(this).setScale(1, RoundingMode.HALF_UP).toDouble()
 
 data class Entry(val name: String, val rating: Double)
+data class Vote(val voter: String, val rating: Int)
