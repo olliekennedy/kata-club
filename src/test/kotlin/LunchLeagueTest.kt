5047f06eac133f8b11e112fd51c3a37079cae235
Ollie Kennedy <okenned3@ford.com>
2025-10-30 20:57:08 +0000

    a user can change their vote but only one counts

--- src/test/kotlin/LunchLeagueTest.kt
+++ src/test/kotlin/LunchLeagueTest.kt
@@ -18,7 +18,7 @@ class LunchLeagueTest {
         val leagueTable = LeagueTable()
 
         leagueTable.vote("Canteen", "me", 8)
-        leagueTable.vote("Canteen", "me", 6)
+        leagueTable.vote("Canteen", "bob", 6)
 
         assertThat(leagueTable.results(), equalTo(listOf(Entry("Canteen", 7.0))))
     }
@@ -28,32 +28,11 @@ class LunchLeagueTest {
         val leagueTable = LeagueTable()
 
         leagueTable.vote("Canteen", "me", 1)
-        leagueTable.vote("Canteen", "me", 10)
-        leagueTable.vote("Canteen", "me", 8)
-        leagueTable.vote("Canteen", "me", 6)
+        leagueTable.vote("Canteen", "bob", 10)
+        leagueTable.vote("Canteen", "sandra", 8)
+        leagueTable.vote("Canteen", "brenda", 6)
 
         assertThat(leagueTable.results(), equalTo(listOf(Entry("Canteen", 6.3))))
-    }
-
-    @Test
-    fun `can get a voting log for a restaurant`() {
-        val leagueTable = LeagueTable()
-
-        leagueTable.vote("Canteen", "me", 1)
-        leagueTable.vote("Canteen", "me", 10)
-        leagueTable.vote("Canteen", "me", 8)
-        leagueTable.vote("Canteen", "me", 6)
-
-        assertThat(
-            leagueTable.votingLogFor("Canteen"),
-            List<Vote>::containsAll,
-            listOf(
-                Vote("me", 1),
-                Vote("me", 10),
-                Vote("me", 8),
-                Vote("me", 6)
-            ),
-        )
     }
 
     @Test
@@ -74,6 +53,16 @@ class LunchLeagueTest {
         assertThat(leagueTable.votingLogFor("Randy's Wing Bar"), equalTo(listOf(Vote("me", 8))))
         assertThat(leagueTable.votingLogFor("Hackney Bridge"), equalTo(listOf(Vote("me", 6))))
     }
+
+    @Test
+    fun `someone can update their rating for a restaurant`() {
+        val leagueTable = LeagueTable()
+
+        leagueTable.vote("Canteen", "me", 6)
+        leagueTable.vote("Canteen", "me", 8)
+
+        assertThat(leagueTable.votingLogFor("Canteen"), equalTo(listOf(Vote("me", 8))))
+    }
 }
 
 data class Vote(val voter: String, val rating: Int)
@@ -81,21 +70,21 @@ data class Vote(val voter: String, val rating: Int)
 class LeagueTable() {
     fun vote(restaurant: String, voter: String, rating: Int) {
         if (restaurant !in votes) {
-            votes[restaurant] = mutableListOf(voter to rating)
+            votes[restaurant] = mutableMapOf(voter to rating)
         } else {
-            votes[restaurant] = (mutableListOf(voter to rating) + votes[restaurant]!!).toMutableList()
+            votes[restaurant]?.set(voter, rating)
         }
     }
 
-    val votes = mutableMapOf<String, MutableList<Pair<String, Int>>>()
+    val votes = mutableMapOf<String, MutableMap<String, Int>>()
 
     fun results(): List<Any> =
         votes.entries.map { (name, votes) ->
-            Entry(name = name, rating = votes.map { it.second }.average())
+            Entry(name = name, rating = votes.map { it.value }.average())
         }.toList()
 
     fun votingLogFor(restaurantName: String): List<Vote> {
-        return votes.getOrElse(restaurantName) { listOf() }.map { Vote(it.first, it.second) }
+        return votes.getOrElse(restaurantName) { mapOf() }.map { Vote(it.key, it.value) }
     }
 }
 
